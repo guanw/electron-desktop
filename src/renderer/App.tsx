ed48ac6ff1b295a0c3be4dc5103918108055f545
guanw <guanw0826@gmail.com>
2022-03-30 21:29:33 -0400

    create note editor with react-markdown

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,20 +1,36 @@
+import { useState } from 'react';
 import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
-
-const Hello = () => {
-  return (
-    <div>
-      <span>Home</span>
-      <Link to="/about">To About</Link>
-    </div>
-  );
-};
 
 const About = () => {
   return (
     <div>
-      <span>About</span>
       <Link to="/">To Home</Link>
+    </div>
+  );
+};
+
+const NoteEditor = () => {
+  const [markdown, setMarkdown] = useState(
+    `# Markdown in react-native is so cool!`
+  );
+
+  return (
+    <div style={{ display: 'flex', height: '100%', overflowY: 'auto' }}>
+      <div style={{ flex: 1, padding: '1rem' }}>
+        <textarea
+          className="form-control"
+          onChange={(event) => {
+            setMarkdown(event.target.value);
+          }}
+          value={markdown}
+        />
+        <Link to="/about">to About</Link>
+      </div>
+      <div style={{ flex: 1, padding: '1rem' }}>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
     </div>
   );
 };
@@ -23,7 +39,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<NoteEditor />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
